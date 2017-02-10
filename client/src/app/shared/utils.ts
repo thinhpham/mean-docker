978bf648d29b18d21d4034e96d7fa5e17bb86d27
Thinh Pham <tpham12345@yahoo.com>
2017-02-09 22:55:18 -0600

    Add public method declaration

--- client/src/app/shared/utils.ts
+++ client/src/app/shared/utils.ts
@@ -1,14 +1,14 @@
 import { Headers, RequestOptions } from '@angular/http';
 
 export class Utils {
-    static createRequestOptions(token: string): RequestOptions {
+    public static createRequestOptions(token: string): RequestOptions {
         if (token)
             return new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json', 'X-Access-Token': token }) });
         else
             return new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json' }) });
     }
 
-    static handleError(error: any): Promise<any> {
+    public static handleError(error: any): Promise<any> {
         console.error('An error occurred', error);
         return Promise.reject(error.message || error);
     }
